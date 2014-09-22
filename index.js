2fdcf31d874127028e8a3587991595ef5a6e3903
Tim Santeford <tsantef@gmail.com>
2014-09-21 19:16:22 -0700

    Step 3: Add commander to index.js

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log('Hello World!');
+var program = require('commander');
+var packageJson = require('./package.json');
+
+// Initialize cli options
+program
+	.version(packageJson.version)
+	.usage('<command> [options]');
+
+// Process Commands
+program.parse(process.argv);
+
+if (typeof program.args.slice(-1)[0] !== 'object') {
+	console.log('Unknown Command: ' + program.args.join(' '));
+	program.help();
+}
