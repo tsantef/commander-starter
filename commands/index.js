906f8e7fd0c4e1792fa789b0a7d972d3c7495a4e
Tim Santeford <tsantef@gmail.com>
2014-09-29 14:32:51 -0700

    Updated command loader

--- commands/index.js
+++ commands/index.js
@@ -5,14 +5,19 @@ module.exports = function commandLoader(program) {
 	'use strict';
 
 	var commands = {};
+	var loadPath = path.dirname(__filename);
 
-	var commandPath = path.dirname(__filename);
-
-	fs.readdirSync(commandPath).forEach(function (filename) {
+	// Loop though command files
+	fs.readdirSync(loadPath).filter(function (filename) {
+		return (/\.js$/.test(filename) && filename !== 'index.js');
+	}).forEach(function (filename) {
 		var name = filename.substr(0, filename.lastIndexOf('.'));
-		if (name !== 'index') {
-			commands[name] = require(path.join(commandPath, filename))(program);
-		}
+
+		// Require command
+		commands[name] = require(path.join(loadPath, filename));
+
+		// Initialize command
+		commands[name](program);
 	});
 
 	return commands;
