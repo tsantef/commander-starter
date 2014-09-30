2e029428e17fdafd6d76ba6af5e6487630d70585
Tim Santeford <tsantef@gmail.com>
2014-09-29 23:07:51 -0700

    Better validation for countdown command

--- commands/countdown.js
+++ commands/countdown.js
@@ -1,14 +1,25 @@
 module.exports = function countdownCommand(program) {
 	'use strict';
 
+	function validateInteger(name) {
+		return function (value) {
+			var parsed = parseInt(value, 10);
+			if (parsed) {
+				return parsed;
+			} else {
+				program.handleError('Invalid ' + name + ': ' + value);
+			}
+		};
+	}
+
 	program
 		.command('countdown <count>')
 		.alias('cd')
 		.description('Countdown timer')
-		.option('-i, --interval <interval>', 'The delay between ticks', parseInt, 1000)
+		.option('-i, --interval <interval>', 'The delay between ticks', validateInteger('Interval'), 1000)
 		.action(function(count, command) {
 
-			count = parseInt(count, 10);
+			count = validateInteger('Count')(count);
 
 			(function tick() {
 				if (count> 0) {
